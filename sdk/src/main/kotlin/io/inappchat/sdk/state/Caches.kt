b5f6311bf63d2fa1601cf35c653dfbbbb1cadc6c
Zaid Daghestani <zaid.daghestani@gmail.com>
2023-03-07 13:38:01 -0500

    cache state

--- sdk/src/main/kotlin/io/inappchat/sdk/state/Caches.kt
+++ sdk/src/main/kotlin/io/inappchat/sdk/state/Caches.kt
@@ -5,14 +5,16 @@
 package io.inappchat.sdk.state
 
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.snapshots.SnapshotStateMap
 
 @Stable
 data class Caches(
-    val users: MutableMap<String, User> = mutableMapOf(),
-    val messages: MutableMap<String, Message> = mutableMapOf(),
-    val groups: MutableMap<String, Group> = mutableMapOf(),
-    val threads: MutableMap<String, Thread> = mutableMapOf(),
-    val threadsByUID: MutableMap<String, Thread> = mutableMapOf(),
-    val threadsByGroup: MutableMap<String, Thread> = mutableMapOf()
+    val users: SnapshotStateMap<String, User> = mutableStateMapOf(),
+    val messages: SnapshotStateMap<String, Message> = mutableStateMapOf(),
+    val groups: SnapshotStateMap<String, Group> = mutableStateMapOf(),
+    val threads: SnapshotStateMap<String, Thread> = mutableStateMapOf(),
+    val threadsByUID: SnapshotStateMap<String, Thread> = mutableStateMapOf(),
+    val threadsByGroup: SnapshotStateMap<String, Thread> = mutableStateMapOf()
 ) {
 }
