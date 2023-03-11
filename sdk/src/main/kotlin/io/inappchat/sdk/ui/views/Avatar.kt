222c1cc37cc6f062286ea0e733cc6d6853c38fcc
Zaid Daghestani <zaid.daghestani@gmail.com>
2023-03-10 23:11:46 -0500

    avatar

--- sdk/src/main/kotlin/io/inappchat/sdk/ui/views/Avatar.kt
+++ sdk/src/main/kotlin/io/inappchat/sdk/ui/views/Avatar.kt
@@ -1,2 +1,57 @@
+/*
+ * Copyright (c) 2023.
+ */
+
 package io.inappchat.sdk.ui.views
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import io.inappchat.sdk.R
+import io.inappchat.sdk.ui.theme.IAC
+
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable
+fun Avatar(url: String?, size: Double = 35.0, group: Boolean = false) {
+    Box(
+        modifier = Modifier
+            .size(size.dp)
+            .background(IAC.colors.softBackground, CircleShape), contentAlignment = Alignment.Center
+    ) {
+        if (url != null) {
+            GlideImage(
+                model = url,
+                contentDescription = "user profile picture",
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clip(CircleShape)
+            )
+        } else {
+            if (group) {
+                GroupPlaceholder(modifier = Modifier.fillMaxSize())
+            } else {
+                Image(
+                    painter = painterResource(id = R.drawable.user_fill),
+                    contentDescription = "user profile picture",
+                    contentScale = ContentScale.Inside,
+                    modifier = Modifier.size((18.0 * size / 35.0).dp),
+                )
+            }
+        }
+    }
+}
