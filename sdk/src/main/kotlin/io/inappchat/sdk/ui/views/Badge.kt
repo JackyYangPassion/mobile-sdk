d361e2d3ecaef289d104d073af1d81535074ec49
Zaid Daghestani <zaid.daghestani@gmail.com>
2023-03-10 23:25:38 -0500

    badge view

---
+++ sdk/src/main/kotlin/io/inappchat/sdk/ui/views/Badge.kt
@@ -0,0 +1,41 @@
+package io.inappchat.sdk.ui.views
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import io.inappchat.sdk.ui.theme.IAC
+
+
+@Composable
+fun Badge(count: Int) {
+    if (count > 0) {
+        Box(
+            modifier = Modifier
+                .size(20.dp)
+                .background(color = IAC.colors.unread, shape = CircleShape)
+                .padding(2.dp)
+        ) {
+            androidx.compose.material3.Text(
+                text = count.toString(),
+                color = Color.White,
+                fontSize = 10.sp,
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun BadgePreview() {
+    Badge(count = 1)
+}
