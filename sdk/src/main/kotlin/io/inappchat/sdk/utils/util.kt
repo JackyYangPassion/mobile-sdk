edca1344dc726f8760d1d2f0eda54313deb6828d
Zaid Daghestani <zaid.daghestani@gmail.com>
2023-03-11 01:52:47 -0500

    more extensions

--- sdk/src/main/kotlin/io/inappchat/sdk/utils/util.kt
+++ sdk/src/main/kotlin/io/inappchat/sdk/utils/util.kt
@@ -4,6 +4,8 @@
 
 package io.inappchat.sdk.utils
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.AnnotatedString
 import io.inappchat.sdk.InAppChat
 import java.util.UUID
 
@@ -11,3 +13,7 @@ fun bundleUrl() = InAppChat.appContext.packageName
 fun uuid() = UUID.randomUUID().toString()
 
 typealias Fn = () -> Unit
+
+
+@Composable
+fun String.annotated() = AnnotatedString(this)
