72f9a6b23b02a5f92f8783d77a72c30b5db2ae21
Zaid Daghestani <zaid.daghestani@gmail.com>
2023-03-11 16:58:02 -0500

    random functions

---
+++ sdk/src/main/kotlin/io/inappchat/sdk/utils/random.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2023.
+ */
+
+package io.inappchat.sdk.utils
+
+
+fun randomImage() = randomImage(300)
+
+fun randomImage( size: Int)
+        = "https://picsum.photos/seed/${faker.random.nextInt()}/$size"
+
+
+fun randomImage( width: Int,  height: Int): {
+    return "https://picsum.photos/seed/${faker.random.nextInt()}/$width/$height"
+}
+
+fun chance(num: Int, outOf: Int): Boolean {
+    return faker.random.nextInt(1, outOf) <= num
+}
+
+fun <T> random(count: Int, item: () -> T): List<T> {
+    val num = faker.random.nextInt(0, count)
+    if (num > 0) {
+        return (0..(num - 1)).map({ item() })
+    }
+    return listOf()
+}
+
+fun <T> randomAmount(from: List<T>): List<T> {
+    if (from.isEmpty()) {return listOf()
+    }
+    var count = faker.random.nextInt(0, from.size)
+    var els = from.toMutableList()
+    if (count > 0) {
+        var ret = mutableListOf<T>()
+        while (count > 0) {
+            val i = faker.random.nextInt(0, els.size)
+            count = count.minus(1)
+            ret.add(els[i])
+            els.remove(i)
+        }
+        return ret
+    } else {
+        return listOf()
+    }
+}
