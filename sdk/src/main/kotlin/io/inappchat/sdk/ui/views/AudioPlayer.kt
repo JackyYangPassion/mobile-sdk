53d863778d7cd0047f0a01cae8fedc443775437f
Zaid Daghestani <zaid.daghestani@gmail.com>
2023-03-13 15:06:56 -0500

    audio player

--- sdk/src/main/kotlin/io/inappchat/sdk/ui/views/AudioPlayer.kt
+++ sdk/src/main/kotlin/io/inappchat/sdk/ui/views/AudioPlayer.kt
@@ -4,23 +4,25 @@
 
 package io.inappchat.sdk.ui.views
 
-import android.view.ViewGroup
-import android.widget.FrameLayout
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
-import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.unit.dp
 import androidx.media3.common.C
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
@@ -28,11 +30,12 @@ import androidx.media3.datasource.DataSource
 import androidx.media3.datasource.DefaultDataSource
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.source.ProgressiveMediaSource
-import androidx.media3.ui.AspectRatioFrameLayout
-import androidx.media3.ui.PlayerView
 import io.inappchat.sdk.R
 import io.inappchat.sdk.ui.theme.IAC.colors
+import io.inappchat.sdk.ui.theme.IAC.fonts
+import io.inappchat.sdk.ui.theme.IAC.theme
 import io.inappchat.sdk.utils.ift
+import kotlinx.coroutines.delay
 
 class AudioUrlProvider(override val values: Sequence<String> = sequenceOf("https://file-examples.com/storage/fe0358100863d05afed02d2/2017/11/file_example_MP3_5MG.mp3")) :
     PreviewParameterProvider<String>
@@ -46,6 +49,12 @@ fun AudioPlayer(
     val context = LocalContext.current
     var playing by remember {
         mutableStateOf(false)
+    }
+    var current by remember {
+        mutableStateOf(0L)
+    }
+    var total by remember {
+        mutableStateOf(0L)
     }
 
     val exoPlayer = remember {
@@ -62,6 +71,21 @@ fun AudioPlayer(
 
                 setMediaSource(source)
                 prepare()
+                val player = this
+                this.addListener(object : Player.Listener {
+                    override fun onPlaybackStateChanged(playbackState: Int) {
+                        if (playbackState == Player.STATE_READY) {
+                            current = player.currentPosition
+                            total = player.contentDuration
+                        }
+                    }
+
+                    override fun onIsPlayingChanged(isPlaying: Boolean) {
+                        playing = isPlaying
+                        current = player.currentPosition
+                        total = player.contentDuration
+                    }
+                })
             }
     }
 
@@ -73,8 +97,7 @@ fun AudioPlayer(
         Box(contentAlignment = Alignment.Center,
             modifier = Modifier
                 .clickable {
-                    if (playing) exoPlayer.pause() else exoPlayer.play()
-                    playing = !playing
+                    if (exoPlayer.isPlaying) exoPlayer.pause() else exoPlayer.play()
                 }
                 .size(44)
                 .background(colors.primary, CircleShape)) {
@@ -86,27 +109,64 @@ fun AudioPlayer(
                         R.drawable.play_fill
                     )
                 ),
-                contentDescription = "play audio"
+                contentDescription = "play audio",
+                tint = Color.White,
+                modifier = Modifier.size(12)
             )
+        }
+
+        Space()
+        Text(text = msToString(current), iac = fonts.body, color = colors.bubbleText)
+        Space()
+        Slider(
+            value = current.toFloat() / 1000.0f,
+            onValueChange = {
+                exoPlayer.pause()
+                current = it.toLong() * 1000L
+            },
+            onValueChangeFinished = {
+                exoPlayer.seekTo(current)
+                exoPlayer.play()
+            },
+            valueRange = 0f..(total.toFloat() / 1000.0f),
+            steps = 1,
+            colors = SliderDefaults.colors(
+                thumbColor = colors.primary,
+                activeTickColor = colors.primary,
+                activeTrackColor = colors.primary
+            ),
+            modifier = Modifier.width(theme.videoPreviewSize.width.dp)
+        )
+        Space()
+        Text(text = msToString(total), iac = fonts.body, color = colors.bubbleText)
+    }
+
+    if (playing) {
+        LaunchedEffect(current) {
+            delay(1000)
+            current += 1000
         }
     }
 
-    DisposableEffect(
-        AndroidView(factory = {
-            PlayerView(context).apply {
-                hideController()
-                useController = false
-                resizeMode = AspectRatioFrameLayout.RESIZE_MODE_ZOOM
-                player = exoPlayer
-                layoutParams = FrameLayout.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT
-                )
-                clipToOutline = true
-            }
-        })
-    ) {
+    DisposableEffect(key1 = "player", effect = {
         onDispose { exoPlayer.release() }
-    }
+    })
 
+}
+
+fun msToString(_time: Long): String {
+    var time = (_time / 1000L)
+    if (time == 0L) {
+        return ""
+    }
+    val seconds = time % 60
+    time = (time - seconds) / 60
+    val minutes = time % 60
+    val hours = (time - minutes) / 60
+    var ret = ""
+    if (hours > 0) {
+        ret += "$hours:"
+    }
+    ret += "$minutes:"
+    return ret + ift(seconds < 10, "0$seconds", "$seconds")
 }
