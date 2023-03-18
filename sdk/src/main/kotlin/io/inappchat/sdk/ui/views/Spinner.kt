8d086ce0231dbf8407a80bc5607ab4f41a31f329
Zaid Daghestani <zaid.daghestani@gmail.com>
2023-03-17 19:20:25 -0500

    spinner

---
+++ sdk/src/main/kotlin/io/inappchat/sdk/ui/views/Spinner.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2023.
+ */
+
+package io.inappchat.sdk.ui.views
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import io.inappchat.sdk.ui.IAC.colors
+
+@Composable
+fun Spinner() {
+  CircularProgressIndicator(color = colors.primary)
+}
+
+@Composable
+fun SpinnerList() {
+  Box(modifier = Modifier
+    .padding(64.dp)
+    .fillMaxSize(), contentAlignment = Alignment.Center) {
+    CircularProgressIndicator(color = colors.primary)
+  }
+}
