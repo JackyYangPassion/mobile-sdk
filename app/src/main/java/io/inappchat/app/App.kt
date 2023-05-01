94c66ee9344e915b6d377996ed1c8472b5704aa2
Zaid Daghestani <zaid.daghestani@gmail.com>
2023-05-01 14:54:58 -0500

    init

--- app/src/main/java/io/inappchat/app/App.kt
+++ app/src/main/java/io/inappchat/app/App.kt
@@ -6,12 +6,14 @@ package io.inappchat.app
 
 import android.app.Application
 import com.giphy.sdk.ui.Giphy
+import io.inappchat.sdk.InAppChat
 
 class App : Application() {
 
   override fun onCreate() {
     super.onCreate()
     Giphy.configure(this, "y29MF80OOqL4I0YQafUDAlcLiRjea4yI")
+    InAppChat.shared.setup(this, "g2minus.ertc.com", "tsxghv2g", true)
+    appState.load(this)
   }
-
 }
