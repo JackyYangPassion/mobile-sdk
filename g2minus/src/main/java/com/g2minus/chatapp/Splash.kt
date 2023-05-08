03093fbc850debb08a346599aa3f6dd771067e17
Zaid Daghestani <zaid.daghestani@gmail.com>
2023-05-08 12:30:12 -0500

    splash screen g2minus

---
+++ g2minus/src/main/java/com/g2minus/chatapp/Splash.kt
@@ -0,0 +1,77 @@
+package com.g2minus.chatapp
+
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import io.inappchat.sdk.InAppChat
+import io.inappchat.sdk.ui.views.Spinner
+
+
+@Composable
+fun Splash(openLogin: () -> Unit, openChat: () -> Unit, content: @Composable (() -> Unit)? = null) {
+    if (content == null) {
+        LaunchedEffect(key1 = InAppChat.shared.loaded, block = {
+            if (InAppChat.shared.loaded) {
+                if (InAppChat.shared.isUserLoggedIn) {
+                    openChat()
+                } else {
+                    openLogin()
+                }
+            }
+        })
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(15.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.g2minus),
+                contentDescription = "InAppChat",
+                modifier = Modifier
+                    .width(120.dp)
+                    .height(120.dp)
+            )
+            if (!InAppChat.shared.loaded || InAppChat.shared.loggingIn)
+                Spinner()
+        }
+        content?.invoke()
+    }
+}
+
+@Preview(
+    name = "Dark mode",
+    group = "UI mode",
+    uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL,
+    showBackground = true,
+    backgroundColor = 0xFF000000
+)
+@Composable
+fun SplashPreview() {
+    MaterialTheme {
+        Splash(openChat = {}, openLogin = {})
+    }
+}
