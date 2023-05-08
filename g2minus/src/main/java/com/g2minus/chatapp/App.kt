d500bc5e72bf64368f166bb708e6307408d1179d
Zaid Daghestani <zaid.daghestani@gmail.com>
2023-05-08 12:30:21 -0500

    auth0 login g2minus

--- g2minus/src/main/java/com/g2minus/chatapp/App.kt
+++ g2minus/src/main/java/com/g2minus/chatapp/App.kt
@@ -4,19 +4,74 @@
 
 package com.g2minus.chatapp
 
+import android.app.Activity
 import android.app.Application
+import android.util.Log
 import com.auth0.android.Auth0
+import com.auth0.android.authentication.AuthenticationException
+import com.auth0.android.callback.Callback
+import com.auth0.android.provider.WebAuthProvider
+import com.auth0.android.result.Credentials
 import com.giphy.sdk.ui.Giphy
 import io.inappchat.sdk.InAppChat
+import io.inappchat.sdk.utils.bg
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
 class App : Application() {
 
-  val auth0: Auth0 = Auth0(this)
+    private lateinit var account: Auth0
 
-  override fun onCreate() {
-    super.onCreate()
-    Giphy.configure(this, getString(R.string.giphy))
+    override fun onCreate() {
+        super.onCreate()
+        Giphy.configure(this, getString(R.string.giphy))
+        account = Auth0(
+            getString(R.string.auth0_client_id),
+            getString(R.string.com_auth0_domain)
+        )
+        InAppChat.shared.setup(this, "g2minus.ertc.com", "tsxghv2g", true)
+    }
 
-    InAppChat.shared.setup(this, "sample.ertc.com", "oj2k3r2x", true)
-  }
+    suspend fun login(activity: Activity) {
+        Log.v("InAppChat Sample", "logging in")
+        val credentials = auth0(activity)
+        val uid = credentials.user.getId() ?: throw Error("Expected a user ID")
+        bg {
+            InAppChat.shared.login(
+                credentials.accessToken,
+                uid,
+                credentials.user.email!!,
+                credentials.user.pictureURL,
+                credentials.user.name,
+                credentials.user.nickname
+            )
+        }
+    }
+
+    companion object {
+        lateinit var app: App
+    }
+
+    private suspend fun auth0(activity: Activity) = suspendCoroutine<Credentials> { continuation ->
+        // Setup the WebAuthProvider, using the custom scheme and scope.
+        WebAuthProvider.login(account)
+            .withScheme(this.packageName)
+            .withScope("openid profile email")
+            // Launch the authentication passing the callback where the results will be received
+            .start(activity, object : Callback<Credentials, AuthenticationException> {
+                // Called when there is an authentication failure
+                override fun onFailure(exception: AuthenticationException) {
+                    // Something went wrong!
+                    continuation.resumeWithException(exception)
+                }
+
+                // Called when authentication completed successfully
+                override fun onSuccess(credentials: Credentials) {
+                    // Get the access token from the credentials object.
+                    // This can be used to call APIs
+                    continuation.resume(credentials)
+                }
+            })
+    }
 }
