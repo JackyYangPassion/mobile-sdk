ca81d394be3b92d07d3805e387171fb2a6afc33c
Zaid Daghestani <zaid.daghestani@gmail.com>
2023-05-09 18:45:19 -0500

    add routing

--- g2minus/src/main/java/com/g2minus/chatapp/MainActivity.kt
+++ g2minus/src/main/java/com/g2minus/chatapp/MainActivity.kt
@@ -3,26 +3,54 @@ package com.g2minus.chatapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.g2minus.chatapp.ui.theme.InappchatTheme
+import io.inappchat.sdk.ui.InAppChatContext
+import io.inappchat.sdk.ui.InAppChatRoutes
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            InappchatTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            val navController = rememberNavController()
+            InAppChatContext {
+                NavHost(navController = navController, startDestination = "splash") {
+                    val openChat = {
+                        navController.navigate("chats") {
+                            popUpTo("splash") {
+                                inclusive = true
+                            }
+                        }
+                    }
+                    composable("splash") {
+                        Splash(openChat = openChat, openLogin = {
+                            navController.navigate("login") {
+                                popUpTo("splash") {
+                                    inclusive = true
+                                }
+                            }
+                        })
+                    }
+                    composable("login") {
+                        Login(openChat, openCreateProfile = {
+                            navController.navigate("create-profile")
+                        })
+                    }
+                    composable("create-profile") {
+                        CreateProfile {
+                            navController.navigate("sign-in")
+                        }
+                    }
+                    composable("sign-in") {
+                        SignIn(openChat)
+                    }
+                    InAppChatRoutes(navController = navController, navGraphBuilder = this)
                 }
             }
         }
