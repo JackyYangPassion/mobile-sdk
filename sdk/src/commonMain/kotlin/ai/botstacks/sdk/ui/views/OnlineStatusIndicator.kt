87c53a8ef93265015fae65b9c4136dfa7a6afafc
Brandon McAnsh <git@bmcreations.dev>
2024-01-23 16:44:30 -0500

    Update OnlineStatusIndicator.kt

--- sdk/src/commonMain/kotlin/ai/botstacks/sdk/ui/views/OnlineStatusIndicator.kt
+++ sdk/src/commonMain/kotlin/ai/botstacks/sdk/ui/views/OnlineStatusIndicator.kt
@@ -3,12 +3,16 @@ package ai.botstacks.sdk.ui.views
 import ai.botstacks.sdk.type.OnlineStatus
 import ai.botstacks.sdk.ui.BotStacks
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -17,9 +21,8 @@ fun OnlineStatusIndicator(
     status: OnlineStatus
 ) {
     Box(
-        modifier = Modifier
-            .background(BotStacks.colorScheme.background, CircleShape)
-            .padding(3.dp)
+        modifier = Modifier.border(width = 2.dp, color = BotStacks.colorScheme.background, CircleShape)
+            .padding(2.dp)
             .size(12.dp)
             .background(
                 when (status) {
