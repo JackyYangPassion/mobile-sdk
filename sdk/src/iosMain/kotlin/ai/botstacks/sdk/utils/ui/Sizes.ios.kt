f9c017055083c7c9d0401a860382a62148285451
Brandon McAnsh <git@bmcreations.dev>
2024-02-08 13:03:48 -0500

    chore: add window size calculation for iOS
    Signed-off-by: Brandon McAnsh <git@bmcreations.dev>

---
+++ sdk/src/iosMain/kotlin/ai/botstacks/sdk/utils/ui/Sizes.ios.kt
@@ -0,0 +1,76 @@
+package ai.botstacks.sdk.utils.ui
+
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.objc.KeyValueObserverProtocol
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.interop.LocalUIViewController
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import kotlinx.cinterop.COpaquePointer
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.ExportObjCClass
+import kotlinx.cinterop.useContents
+import platform.Foundation.NSKeyValueObservingOptionNew
+import platform.Foundation.addObserver
+import platform.Foundation.removeObserver
+import platform.UIKit.UIViewController
+import platform.darwin.NSObject
+
+@OptIn(ExperimentalForeignApi::class)
+@ExperimentalMaterial3WindowSizeClassApi
+@Composable
+actual fun calculateScreenSize(): DpSize {
+    val uiViewController = LocalUIViewController.current
+
+    var screenSize by remember(uiViewController) {
+        mutableStateOf(DpSize.Zero)
+    }
+
+    DisposableEffect(uiViewController) {
+        val observer = ObserverObject {
+            screenSize = uiViewController.getViewFrameSize()
+        }
+
+        uiViewController.view.layer.addObserver(
+            observer = observer,
+            forKeyPath = "bounds",
+            options = NSKeyValueObservingOptionNew,
+            context = null,
+        )
+
+        onDispose {
+            uiViewController.view.layer.removeObserver(
+                observer = observer,
+                forKeyPath = "bounds",
+            )
+        }
+    }
+
+    return screenSize
+}
+
+@OptIn(ExperimentalForeignApi::class)
+private fun UIViewController.getViewFrameSize(): DpSize = view.frame().useContents {
+    // iOS returns density independent pixels, rather than raw pixels
+    DpSize(size.width.dp, size.height.dp)
+}
+
+@OptIn(ExperimentalForeignApi::class)
+@ExportObjCClass
+private class ObserverObject(
+    private val onChange: () -> Unit,
+) : NSObject(), KeyValueObserverProtocol {
+    override fun observeValueForKeyPath(
+        keyPath: String?,
+        ofObject: Any?,
+        change: Map<Any?, *>?,
+        context: COpaquePointer?,
+    ) {
+        onChange()
+    }
+}
