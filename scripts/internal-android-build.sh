70f88523929b83de2f21d60775e7fb07d734c4b8
Brandon McAnsh <git@bmcreations.dev>
2024-02-27 08:48:56 -0500

    build: make incremental internal builds scriptable
    Signed-off-by: Brandon McAnsh <git@bmcreations.dev>

--- scripts/internal-android-build.sh
+++ scripts/internal-android-build.sh
@@ -4,5 +4,15 @@ date="$(date '+%m.%d.%y')"
 
 ./gradlew assembleDebug
 
+build_num=$1
+
+if [ -z "$build_num" ]
+then
+   apk_name="sample-android-${date}-debug.apk"
+else
+  apk_name="sample-android-${date}_$build_num-debug.apk"
+fi
+
 outputDir="$(pwd)/sample-android/build/outputs/apk/debug"
-mv "${outputDir}/sample-android-debug.apk" "${outputDir}/sample-android-${date}-debug.apk"
+
+mv "${outputDir}/sample-android-debug.apk" "${outputDir}/${apk_name}"
